37b723c1b9af75435f7560cc926d316b395bcce9
Amaury <>
2022-09-25 19:38:44 +0200

    chore(i3): update config

--- i3/setup.sh
+++ i3/setup.sh
@@ -4,3 +4,6 @@ source ../utils.sh
 
 mkdir -p $HOME/.config/i3/
 try_backup_and_setup_file $CONFIGS_FOLDER/i3/config $HOME/.config/i3/config
+
+mkdir -p $HOME/.config/i3status/
+try_backup_and_setup_file $CONFIGS_FOLDER/i3/i3status/config $HOME/.config/i3status/config
