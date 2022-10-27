1d642575f93875809e0fbb62b30688723a69808b
Amaury <>
2022-10-27 23:35:27 +0200

    chore: update setup scripts

--- init.sh
+++ init.sh
@@ -2,14 +2,6 @@
 
 source utils.sh
 
-if [[ $(pwd) != "$HOME/.dotfiles" ]]; then
-  if [[ ! -f $HOME/.dotfiles ]] && [[ ! -d $HOME/.dotfiles ]]; then
-    ln -s $(pwd) $HOME/.dotfiles
-  else
-    echo "$HOME/.dotfiles alreay exists"
-  fi
-fi
-
 for item in ./*; do
   if [[ -f $item/init.sh ]]; then
     cd $item
--- setup.sh
+++ setup.sh
@@ -2,6 +2,14 @@
 
 source ./utils.sh
 
+if [[ $(pwd) != "$HOME/.dotfiles" ]]; then
+  if [[ ! -f $HOME/.dotfiles ]] && [[ ! -d $HOME/.dotfiles ]]; then
+    ln -s $(pwd) $HOME/.dotfiles
+  else
+    echo "$HOME/.dotfiles alreay exists"
+  fi
+fi
+
 for item in ./*; do
   if [[ -f $item/setup.sh ]]; then
     cd $item
