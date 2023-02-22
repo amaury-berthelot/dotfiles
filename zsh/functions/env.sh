299a76ac9f616d3082c8d66a4ff56f7edeae6951
Amaury <>
2023-02-22 08:58:11 +0100

    zsh: update aliases and functions

--- zsh/functions/env.sh
+++ zsh/functions/env.sh
@@ -4,4 +4,8 @@ function ,env {
   if [[ -f .env.sh ]]; then
     source .env.sh
   fi
+
+  if [[ -f .nvmrc ]] && [[ "$(command -v nvm)" != "" ]]; then
+    nvm use
+  fi
 }
--- zsh/functions/jump.sh
+++ zsh/functions/jump.sh
@@ -2,6 +2,7 @@
 
 function ,jump() {
   local DATA_PATH="$HOME/.jump_locations.sh"
+  local HISTORY_PATH="$HOME/.jump_history"
 
   # Add ~/.jump_locations.sh with entries like
   # local docs=/home/me/Documents
@@ -11,14 +12,23 @@ function ,jump() {
       source $DATA_PATH
       local target="${(P)1}"
 
-      if [ -n $target ]; then
+      if [[ $target != "" ]]; then
         cd $target
+        ,env
+      elif [[ -d $1 ]]; then
+        cd $1
+        pwd >> $HISTORY_PATH
+        cat $HISTORY_PATH | sort | uniq > $HISTORY_PATH
+        ,env
       else
         echo "cannot jump"
       fi
     else
       echo "no jump locations"
     fi
+  elif [[ -f $HISTORY_PATH ]] && [[ $# -eq 0 ]]; then
+    cd $(cat $HISTORY_PATH | sort | uniq | fzf)
+    ,env
   elif [[ $# -eq 2 ]] && [[ $1 == "add" ]]; then
     echo "local $2=$(pwd)" >> $DATA_PATH
   else
