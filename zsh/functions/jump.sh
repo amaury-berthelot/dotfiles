32d3c408e41200aab5188a3fa585ef200cdb71f5
Amaury <>
2023-03-14 17:03:10 +0100

    zsh: update jump function

--- zsh/functions/jump.sh
+++ zsh/functions/jump.sh
@@ -7,34 +7,43 @@ function ,jump() {
   # Add ~/.jump_locations.sh with entries like
   # local docs=/home/me/Documents
 
-  if [ $# -eq 1 ]; then
-    if [ -f $DATA_PATH ]; then
-      source $DATA_PATH
-      local target="${(P)1}"
-
-      if [[ -f $HISTORY_PATH ]] && [[ $1 == '$' ]]; then
-        ,clear_env
-        cd $(cat $HISTORY_PATH | sort | uniq | fzf)
-        ,env
-      elif [[ $target != "" ]]; then
-        ,clear_env
-        cd $target
-        ,env
-      else
-        ,clear_env
-        cd $1
-        pwd >> $HISTORY_PATH
-        cat $HISTORY_PATH | sort | uniq > $HISTORY_PATH
-        ,env
-      fi
-    else
-      echo "no jump locations"
-    fi
-  elif [[ $# -eq 2 ]] && [[ $1 == "add" ]]; then
-    echo "local $2=$(pwd)" >> $DATA_PATH
-  else
+  if [[ $# -eq 0 ]]; then
     ,clear_env
     cd
+    return
+  fi
+
+  local trigger_env_after_change=1
+  if [[ $# -ge 1 ]] && [[ $1 == "!" ]]; then
+    trigger_env_after_change=0
+    shift
+  fi
+
+  if [[ -f $DATA_PATH ]]; then
+    source $DATA_PATH
+  fi
+
+  if [[ $# -eq 2 ]] && [[ $1 == "add" ]]; then
+    echo "local $2=$(pwd)" >> $DATA_PATH
+    echo "$(pwd) added as $2"
+    return
+  fi
+
+  local target="${(P)1}"
+
+  if [[ $1 == "$" ]]; then
+    ,clear_env
+    cd $(cat $HISTORY_PATH | sort | uniq | fzf)
+  elif [[ $target != "" ]]; then
+    ,clear_env
+    cd $target
+  elif [[ -d $1 ]]; then
+    ,clear_env
+    cd $1
+    pwd >> $HISTORY_PATH
+  fi
+
+  if [[ $trigger_env_after_change -eq 1 ]]; then
     ,env
   fi
 }
